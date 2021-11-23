e311951ebd8c06ba9ab55709db336ba067c8572c
pavelihno <crazyyellow@yandex.ru>
2021-11-23 21:17:49 +0300

    Login сделан. Минорные исправления в index

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,6 @@
-document.getElementById('check').onclick = function() {
-    let login = document.getElementById('login').value;
-    let password = document.getElementById('password').value;
+document.getElementById('enter').onclick = function() {
+    let login = document.getElementById('l-login').value;
+    let password = document.getElementById('l-password').value;
 
     if (login.length && password.length) {
         window.location.href = "profile.html";
